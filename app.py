eec262811257c3db5aca3eb8ec5e624360489c9a
christian didelot <didelot.c@gmail.com>
2020-02-23 11:14:50 +0100

    v1 with logo

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import glob
+import logging
 import os
 
 import qrcode as qrcode
@@ -6,8 +7,8 @@ from PIL import Image
 from flask import Flask, flash, request, redirect, render_template
 from werkzeug.utils import secure_filename
 
-WALL_FOLDER = "static/photo"
-BRUT_FOLDER = 'brut'
+WALL_FOLDER = "static/photowall"
+DSLR_FOLDER = 'brut'
 UPLOAD_FOLDER = 'static/upload/'
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
 
@@ -16,42 +17,55 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.secret_key = 'super secret key'
 app.config['SESSION_TYPE'] = 'filesystem'
 
+
 def diff(first, second):
     second = set(second)
     return [item for item in first if item not in second]
 
 
-@app.route('/')
-@app.route('/index')
-def show_index():
-    listb = glob.glob1(BRUT_FOLDER, '*.jpg')
+def make_picture():
+
+    listb = glob.glob1(DSLR_FOLDER, '*.jpg')
     listu = glob.glob1(UPLOAD_FOLDER, "*.jpg")
     listf = glob.glob1(WALL_FOLDER, '*.jpg')
 
-    listt = diff(listb, listf)
-    print(f"found {len(listt)} photo to resize")
-    print(f"liste to resize: {listt}")
-
-    for img in listt:
+    listtw = diff(listb, listf)
+    listtu = diff(listu, listf)
+    print(f"found {len(listtw)} from brut to resize")
+    print(f"liste to resize: {listtw}")
+    print(f"found {len(listtu)} from upload to resize")
+    print(f"liste to resize: {listtw}")
+    for img in listtw:
         print(f"resizing {img}")
-        image = Image.open(os.path.join('brut', img))
+        image = Image.open(os.path.join(DSLR_FOLDER, img))
         size = (600, 600)
         image.thumbnail(size)
-        path_save = ('static/photo')
+        path_save = (WALL_FOLDER)
         image.save(os.path.join(path_save, img))
 
-    for img in listu:
+    for img in listtu:
         print(f"resizing {img}")
-        image = Image.open(os.path.join('static/upload', img))
+        image = Image.open(os.path.join(UPLOAD_FOLDER, img))
         size = (600, 600)
         image.thumbnail(size)
-        path_save = ('static/photo')
+        path_save = (WALL_FOLDER)
         image.save(os.path.join(path_save, img))
 
 
-    listw = sorted(glob.glob(os.path.join(WALL_FOLDER, '*.jpg')))
+    return  listf
 
+
+@app.route('/')
+@app.route('/index')
+def show_index():
+    listw =make_picture()
     return render_template("index.html", images=listw)
+
+
+@app.route('/admin')
+def show_admin():
+    listw = make_picture()
+    return render_template("admin.html", images=listw)
 
 
 def allowed_file(filename):
@@ -76,36 +90,42 @@ def upload_file():
             filename = secure_filename(file.filename)
             file.save(os.path.join(UPLOAD_FOLDER, filename))
             # return redirect(url_for('upload_file', filename=filename))
-            return 'file correctly upload'
+            # return 'file correctly upload'
+            return redirect("/")
 
-    return '''
-    <!doctype html>
-    <title>Upload new File</title>
-    <h1>Upload new File</h1>
-    <form method=post enctype=multipart/form-data>
-      <input type=file name=file>
-      <input type=submit value=Upload>
-    </form>
-    '''
-@app.route('/qrcode')
-def gen_qrcode():
+    return render_template("upload.html")
+
+@app.route('/qrcode/<data>')
+def gen_qrcode(data):
     qr = qrcode.QRCode(
-        version=12,
+        version=1,
         error_correction=qrcode.constants.ERROR_CORRECT_H,
-        box_size=2,
-        border=8
+        box_size=12,
+        border=3
     )
-    qr.add_data('http://localhost')
+    qr.add_data(data)
     qr.make()
-    img = qr.make_image(fill_color="black", back_color="#23dda0")
-    img.save('static/qrcode/qrcode_test2_2.png')
-    return 'ok'
+    img = qr.make_image(fill_color="black", back_color="white")
+    img.save('static/qrcode/qr_code.png')
+    return f'qr_code generate for {data}'
+
 
 @app.route('/delete/<img>')
 def delete_pic(img):
-    os.unlink(os.path.join(UPLOAD_FOLDER, img))
-    os.unlink(os.path.join(BRUT_FOLDER, img))
+    img = img.split('/')[-1]
+    print(f"delete img {img}")
+    try:
+        os.unlink(os.path.join(UPLOAD_FOLDER, img))
+    except Exception as e:
+        logging.debug(e)
+    try:
+        os.unlink(os.path.join(DSLR_FOLDER, img))
+    except Exception as e:
+        logging.debug(e)
+
+
     os.unlink(os.path.join(WALL_FOLDER, img))
+    return f'picture {img} deleted '
 
 @app.after_request
 def add_header(response):
