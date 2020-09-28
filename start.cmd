56dd4ad229eb09f965983bab5d936f21d6ae2bc3
christian didelot <didelot.c@gmail.com>
2020-09-28 20:20:07 +0200

    update

--- start.cmd
+++
@@ -1,6 +0,0 @@
-@echo off
-set /p EVENT_DIR="ENTER EVENT NAME: "
-set EVENT_DIR
-python app.py
-cd C:\Users\gumpy1\Documents\GitHub\photowall\
-python C:\Users\gumpy1\Documents\GitHub\photowall\app.py
--- start_dev.cmd
+++ start_dev.cmd
@@ -1,6 +1,7 @@
 @echo off
-
+set /p EVENT_DIR="ENTER EVENT NAME: "
 set EVENT_DIR
+set FLASK_ENV=development
 python app.py
 cd C:\Users\gumpy1\Documents\GitHub\photowall\
 python C:\Users\gumpy1\Documents\GitHub\photowall\app.py
